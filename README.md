0dbd49a6ff9cfce7594b74e549cb8024959c9f46
Camil Băncioiu <ioan.camil.bancioiu@gmail.com>
2020-08-09 15:22:50 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,65 @@
+# Shelladder
+`shelladder` enables you to call Python functions as if they were Bash
+functions or normal commands.
+
+
+# How it works
+You begin by writing your Python functions in a module, e.g. `myfunctions.py`.
+This module _must not_ contain the usual construct `if __name__ == "__main__"`.
+Just write the functions, because `shelladder` will handle their calling.
+
+Let's say you would like a Python function called `printdir()` to be accessible
+to you in the terminal, as if it were a normal command. You would write a file
+containing this:
+
+```python
+import os
+
+
+# export
+def printdir():
+    print(os.getcwd())
+```
+
+Note the `# export` line just above the line `def printdir():`. Adding `#
+export` before defining a function marks it for export into Bash, so
+`shelladdder` knows you want to call it. Function definitions that don't have
+the `# export` mark will remain "private" to the module, i.e. they can be
+called by other exported functions, but not from Bash.
+
+Once you're happy with your Python functions, save your file. For example,
+`addertest.py` in your current folder. Now run this command:
+
+```
+python3 /some/where/shelladder/shelladder.py addertest.py
+```
+
+What `shelladder` does is that it creates a new folder in your `$HOME` called
+`.shelladder`, where it will deploy your `addertest.py` module (with minor
+modifications!), and it will create a companion Bash script, called
+`addertest.sh`. By sourcing `addertest.sh` into your shell, you gain access to
+all the `# export`-ed functions in `addertest.py`. Neat, right?
+
+Try it:
+
+```
+source ~/.shelladder/addertest.sh
+```
+
+Observe that `shelladder` will print the location of this companion Bash
+script, which allows you to do everything in one command:
+
+```
+source $(python3 shelladder.py addertest.py)
+```
+
+Just make sure you run the above command everytime you edit `addertest.py`.
+
+_You need to specify the path to `shelladder.py`, because it's just a Python
+script, not an executable._ Or add an alias to `~/.bashrc`:
+
+```
+alias adder="python3 /some/where/shelladder/shelladder.py"
+```
+
+Now you can do `adder addertest.py` anytime, from anywhere.
---
+++ shelladder.py
@@ -0,0 +1,92 @@
+import argparse
+from pathlib import Path
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('inputfilename', type=str, default=None,
+                        action='store',
+                        help='The input file name')
+    return parser
+
+
+def build_py_module(inputfile, outputfile_py, mark='export'):
+    mark = '# ' + mark
+    new_definition = False
+    functions = list()
+    for line in inputfile:
+        outputfile_py.write(line)
+        if line.startswith(mark):
+            new_definition = True
+            continue
+        if new_definition:
+            function = parse_py_definition_line(line)
+            functions.append(function)
+            new_definition = False
+
+    return functions
+
+
+def parse_py_definition_line(line):
+    if line[0:4] != 'def ':
+        raise ValueError('Line after mark does not start with def: {}'
+                         .format(line))
+    line = line[4:]
+    parts = line.partition('(')
+    if parts[1] == '':
+        raise ValueError('Could not read definition line: {}'.format(line))
+    return parts[0]
+
+
+def write_py_header(output):
+    output.write('import sys\n')
+
+
+def write_sh_wrappers(output, module, functions):
+    for function in functions:
+        output.write("function {} {{\n".format(function))
+        output.write("    python3 {} {}\n".format(module, function))
+        output.write("}")
+
+
+def write_py_main(output, functions):
+    output.write("\n\n")
+    output.write("if __name__ == '__main__':\n")
+    output.write("    if len(sys.argv) == 0:\n")
+    output.write("        raise ValueError('Function name required')\n")
+    output.write("\n")
+    output.write("    functions = dict()\n")
+
+    for function in functions:
+        output.write("    functions['{0}'] = {0}\n".format(function))
+
+    output.write("    function_to_call = sys.argv[1]\n")
+    output.write("    del sys.argv[1]\n")
+    output.write("    function = functions[function_to_call]\n")
+    output.write("    result = function()\n")
+    output.write("    if result is not None:\n")
+    output.write("        print(result)\n")
+
+
+if __name__ == '__main__':
+    parser = create_parser()
+    arguments = parser.parse_args()
+
+    shelladder = Path.home() / '.shelladder'
+    shelladder.mkdir(parents=True, exist_ok=True)
+
+    inputfile = Path(arguments.inputfilename)
+    outputfile_py = shelladder / inputfile.name
+    outputfile_sh = shelladder / inputfile.with_suffix('.sh').name
+
+    with inputfile.open('r') as inputfile:
+        with outputfile_py.open('w') as output_py:
+            functions = None
+            write_py_header(output_py)
+            functions = build_py_module(inputfile, output_py)
+            write_py_main(output_py, functions)
+
+        with outputfile_sh.open('w') as output_sh:
+            write_sh_wrappers(output_sh, outputfile_py, functions)
+
+    print(outputfile_sh)
